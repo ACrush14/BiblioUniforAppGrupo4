30f3a2f9905e0309dcd72c78c97f39062ff07000
Isabele (Inko) Aragão Guerra <isabelearagao102@gmail.com>
2026-04-30 18:28:58 -0300

    Create RF14TelaVerMaisLivro.kt

---
+++ app/src/main/java/com/example/bibliounifor/ui/screens/RF14TelaVerMaisLivro.kt
@@ -0,0 +1,16 @@
+package com.example.teste
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RF14TelaVerMaisLivro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.telarf14_telavermaislivro)
+        
+    }
+}
