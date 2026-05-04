e415e79f2a84f846af5d521844251d12cbeeac00
Loïe Fermanian <loiefermanian@gmail.com>
2026-05-04 09:25:39 -0300

    merge main na minha branch. Telas conectadas da telarf30_dashboard_adm.xml

--- app/src/main/java/com/example/bibliounifor/TelaRF30DashboardADM.kt
+++ app/src/main/java/com/example/bibliounifor/TelaRF30DashboardADM.kt
@@ -12,12 +12,38 @@ class TelaRF30DashboardADM : AppCompatActivity() {
         setContentView(R.layout.telarf30_dashboard_adm)
 
         val btnTelaCRUD = findViewById<Button>(R.id.btnTelaCRUD)
+        val btnVerAlugueis = findViewById<Button>(R.id.btnVerAlugueis)
+        val btnVerAtrasos = findViewById<Button>(R.id.btnVerAtrasos)
+        val btnVerCadastros = findViewById<Button>(R.id.btnVerCadastros)
+        val btnVerSolicitacoes = findViewById<Button>(R.id.btnVerSolicitacoes)
+        val btnVerRenovacoes = findViewById<Button>(R.id.btnVerRenovacoes)
 
         btnTelaCRUD.setOnClickListener {
             // Alterado para ir para a TelaRF34LivrosCRUD conforme solicitado
             val intent = Intent(this, TelaRF34LivrosCRUD::class.java)
             startActivity(intent)
         }
+
+        btnVerAlugueis.setOnClickListener {
+            val intent = Intent(this, TelaRF38ListaAlugueisADM::class.java)
+            startActivity(intent)
+        }
+
+        btnVerAtrasos.setOnClickListener {
+            val intent = Intent(this, TelaRF36FinanceiroADM::class.java)
+            startActivity(intent)
+        }
+
+        btnVerCadastros.setOnClickListener {
+            val intent = Intent(this, TelaRF35CadastroDeLivros::class.java)
+            startActivity(intent)
+        }
+
+        btnVerSolicitacoes.setOnClickListener {
+            val intent = Intent(this, TelaRF33Solicitacoes::class.java)
+            startActivity(intent)
+        }
+
     }
 
 }
