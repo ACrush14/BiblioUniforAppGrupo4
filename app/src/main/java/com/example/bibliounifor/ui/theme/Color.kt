5db2b64ddd53431088c1024be0643a61dd215ac4
Anderson (Crush) de Lima <96736515+ACrush14@users.noreply.github.com>
2026-05-10 20:51:58 -0300

    Delete app/src/main/java/com/example/bibliounifor directory
    Mudado para receber branch de Brena

--- app/src/main/java/com/example/bibliounifor/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bibliounifor.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val BiblioCyan = Color(0xFF87DEDE)
-val BiblioDark = Color(0xFF1B3434)
-val BiblioBg = Color(0xFFF5F5F5)
-val BiblioRed = Color(0xFFD9534F)
